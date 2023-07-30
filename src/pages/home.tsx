0b1c6977a1f64052f56861dc4e0a91adb75b293f
jkpark104 <jkrang104@gmail.com>
2023-07-30 21:47:48 +0900

    meta-tag update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,19 @@
+import { Helmet } from "react-helmet-async";
+
 function Home() {
   return (
     <>
+      <Helmet>
+        <meta property="og:title" content="Others" />
+        <meta property="og:description" content="Others" />
+        <meta
+          property="og:image"
+          content="https://www.searchenginejournal.com/wp-content/uploads/2019/10/25-of-the-best-examples-of-home-pages-5dc504205de2e-1520x800.webp"
+        />
+        <meta property="og:image:width" content="1600" />
+        <meta property="og:image:height" content="900" />
+        <title>Home</title>
+      </Helmet>
       <h1>Home</h1>
     </>
   );
