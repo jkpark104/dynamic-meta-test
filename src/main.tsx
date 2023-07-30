69e9aac46e65b436bc5784b542d69e10f4836bc8
jkpark104 <jkrang104@gmail.com>
2023-07-30 22:19:16 +0900

    push

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,26 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { HelmetProvider } from "react-helmet-async";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById("root")!;
+
+if (rootElement.hasChildNodes()) {
+  const root = ReactDOM.hydrateRoot(
+    document.getElementById("root")!,
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+  console.log(root);
+} else {
+  const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+  root.render(
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  );
+}
