0e8142e794553850e45a0ff46439ac3bfe93e913
jkpark104 <jkrang104@gmail.com>
2023-07-30 22:34:52 +0900

    test3: meta-tag

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,8 @@ function Home() {
   return (
     <>
       <Helmet>
-        <meta property="og:title" content="Others" />
-        <meta property="og:description" content="Others" />
+        <meta property="og:title" content="Home" />
+        <meta property="og:description" content="Home" />
         <meta
           property="og:image"
           content="https://www.searchenginejournal.com/wp-content/uploads/2019/10/25-of-the-best-examples-of-home-pages-5dc504205de2e-1520x800.webp"
